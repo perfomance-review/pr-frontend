25d1882c7590061e97fb61aabb6cc35756f74c81
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2022-06-14 22:14:39 +0500

    getCurrentUser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -9,6 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+const store = createStore(reducer)
 
 root.render(
   <React.StrictMode>
---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+    user: {
+        userId:"",
+        firstName:"",
+        secondName:"",
+        role:""
+    }
+}
+
+export default function reducer(state = initialState, action){
+    switch(action.type){
+        case 'ChangeUser':
+            return {
+                ...state, user: action.payload
+            }
+        default:
+            return state
+    }
+}
