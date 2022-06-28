b4336fbb3f35b0a42501aa2d3d52eb347adb4097
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2022-06-28 21:26:09 +0500

    Удаляемая кука

--- src/API/Cookies.js
+++ src/API/Cookies.js
@@ -6,7 +6,7 @@ export default class Cookies {
   }
 
   static deleteCookie(cookieName) {
-    document.cookie = cookieName + '=123' + ';max-age=-1';
+    document.cookie = cookieName + '=0' + ';max-age=-1';
   }
 
   static setCookie(cookieName, cookieValue) {
